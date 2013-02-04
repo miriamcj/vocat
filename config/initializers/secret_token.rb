bbbbc9f6ccf1643b378f274debb57d429243c34b
Zach Davis <zach@castironcoding.com>
2013-02-04 11:21:36 -0800

    [FEATURE] Initial commit of VOCAT3. Woot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vocat::Application.config.secret_token = '8d6481501ac2147ad8d650370512b46353dacfe562c57648b5f1a9cbe6d749a2b38e069e127f925c10da810825b762977ea2ba4adccc61979acc2285aae3e5a9'
