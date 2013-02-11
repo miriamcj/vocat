6582b5e0c7c27ba14fe9476c3785f122cf934e52
Peter Soots <peter@castironcoding.com>
2013-02-11 12:23:21 -0800

    [CHORE] Renamed old AssignmentSubmission migration.

--- db/migrate/20130205183635_create_assignment_submissions_DEPRECATED.rb
+++ db/migrate/20130205183635_create_assignment_submissions_DEPRECATED.rb
@@ -1,4 +1,4 @@
-class CreateAssignmentSubmissions < ActiveRecord::Migration
+class CreateAssignmentSubmissions_DEPRECATED < ActiveRecord::Migration
   def change
     create_table :assignment_submissions do |t|
       t.string :name
