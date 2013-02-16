39b29f53734f813b802bda3dffe18f823b2c6a1d
Peter Soots <peter@castironcoding.com>
2013-02-15 16:47:38 -0800

    [CHORE] Created a form_errors helper in the application_helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def form_errors(resource)
+    return unless resource.errors.messages.length > 0
+    if resource.errors.messages.length == 1
+      error = resource.errors.full_messages.first
+      return content_tag :div, error, :class => "alert alert-error"
+    end
+    p = content_tag :p, "Please fix the following errors:"
+    errors = resource.errors.full_messages.map { |msg| content_tag(:li, msg, nil, false) }.join
+    list = content_tag :ul, errors, nil, false
+    content_tag :div, p+list, :class => "alert alert-error"
+  end
 end
