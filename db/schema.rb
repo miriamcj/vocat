ac4e9ec2f9c33618ab3bf5233ccbbe24a712887e
Zach Davis <zach@castironcoding.com>
2013-03-24 13:45:50 -0700

    [FEATURE] Adds hstore gem to gemfile and updates schema to reflect papertrail migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130215223102) do
+ActiveRecord::Schema.define(:version => 20130324180224) do
 
   create_table "attachments", :force => true do |t|
     t.string   "media_file_name"
@@ -138,5 +138,16 @@ ActiveRecord::Schema.define(:version => 20130215223102) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["organization_id"], :name => "index_users_on_organization_id"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",  :null => false
+    t.integer  "item_id",    :null => false
+    t.string   "event",      :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
