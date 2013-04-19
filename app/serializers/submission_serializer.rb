c020bcfd372e0aa50f859a88ce14358c2a4c5930
Scott Mills <scott@castironcoding.com>
2013-04-19 11:54:38 -0700

    [CHORE] Modifies serialized course data

--- app/serializers/submission_serializer.rb
+++ app/serializers/submission_serializer.rb
@@ -1,7 +1,7 @@
 class SubmissionSerializer < ActiveModel::Serializer
   attributes  :id, :name, :url, :thumb, :course_name, :course_name_long, :project_name,
               :course_id, :project_id, :creator_name, :creator_id, :current_user_is_owner,
-              :current_user_can_evaluate
+              :current_user_can_evaluate, :course_department, :course_section, :course_number
 
   def current_user_can_evaluate
     Ability.new(scope).can?(:evaluate, object)
