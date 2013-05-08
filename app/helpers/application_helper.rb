0ac79e293b96c34deec511d80073bc56064842bc
Zach Davis <zach@castironcoding.com>
2013-05-08 15:38:15 -0700

    [CHORE] Removed some deprecated code from the app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,49 +28,4 @@ module ApplicationHelper
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
   end
 
-  # LINK HELPERS
-  #
-  # Since we have namespaces tied to user roles, we can create shortcuts for the
-  # existing routes that figure out the namespace automatically
-  #
-  # For example, `evaluator_organization_project` => `organization_project` and
-  # `edit_admin_organization_project` => `edit_organization_project`
-  #
-
-  # Get a list of route names
-  #names = Array.new
-  #Rails.application.routes.named_routes.routes.each { |name, route| names << name.to_s }
-  #
-  ## Remove namespace from route, and save old route
-  ## with a '#' replacing the namespace
-  #routes = Hash.new
-  #names.each do |name|
-  #  val = name.gsub("admin", "#")
-  #  val = val.gsub("creator", "#")
-  #  val = val.gsub("evaluator", "#")
-  #  if val["_#_"]
-  #    key = val.gsub("_#_", "_")
-  #  else
-  #    key = val.gsub("#_", "")
-  #  end
-  #  if val["#"]
-  #    routes[key] = val
-  #  end
-  #end
-  #
-  #%w(path url).each do |type|
-  #
-  #  # root_path_for(user, organization) -> organization_courses
-  #  define_method "root_#{type}_for" do |user, organization|
-  #    send "#{user.role}_organization_courses_#{type}", organization
-  #  end
-  #
-  #  # Define the link helper
-  #  routes.each do |route, namespace_route|
-  #    define_method "#{route}_#{type}" do |*args|
-  #      r = namespace_route.gsub("#", current_user.role)
-  #      send "#{r}_#{type}", *args
-  #    end
-  #  end
-  #end
 end
