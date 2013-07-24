1cfe2c3aac91d1cb5d9ce7c2c286171f6ca94cf3
Zach Davis <zach@castironcoding.com>
2013-07-23 17:02:34 -0700

    [CHORE] Adds application.js to application.rb config for asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,9 @@ module Vocat
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += %w(
+      application.js
+    )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
