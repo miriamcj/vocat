40f27a0db8455caf2ca78cd3cad9ae8112f3b250
Zach Davis <zach@castironcoding.com>
2013-08-14 10:34:52 -0700

    [BUGFIX] Adds missing s to discussion_post_count in brief submission serializer

--- app/serializers/brief_submission_serializer.rb
+++ app/serializers/brief_submission_serializer.rb
@@ -1,7 +1,7 @@
 class BriefSubmissionSerializer < ActiveModel::Serializer
   attributes  :id, :thumb, :creator_id, :project_id, :instructor_score_percentage, :published, :has_video?,
               :evaluated_by_instructor?, :current_user_percentage, :current_user_has_evaluated?, :current_user_evaluation_published?,
-              :current_user_evaluation, :discussion_post_count
+              :current_user_evaluation, :discussion_posts_count
 
   def current_user_percentage
     object.user_score_percentage(scope)
