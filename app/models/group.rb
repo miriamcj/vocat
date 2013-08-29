af2d7384d8a581e170fa7b5ed5684fd4d7d5f5b5
Zach Davis <zach@castironcoding.com>
2013-08-29 16:13:35 -0700

    [BUGFIX] Adds dependent destroy to submisisions association on group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ActiveRecord::Base
   attr_accessible :course_id, :name, :creator_ids
   belongs_to :course
 
-  has_many :submissions, :as => :creator
+  has_many :submissions, :as => :creator, :dependent => :destroy
 
   has_and_belongs_to_many :creators, :class_name => "User", :join_table => "groups_creators"
 
