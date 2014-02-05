2a6b6ee7e9bb91a2db57b0e0ab3d485328076ed3
Zach Davis <zach@castironcoding.com>
2014-02-05 10:45:02 -0800

    [CHORE] Adds is_ldap_user to users object

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131211224650) do
+ActiveRecord::Schema.define(version: 20140205163110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -224,6 +224,7 @@ ActiveRecord::Schema.define(version: 20131211224650) do
     t.string   "city"
     t.string   "state"
     t.string   "country"
+    t.boolean  "is_ldap_user"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
