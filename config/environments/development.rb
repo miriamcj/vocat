e645a9fba417908746e00e7cf17685030dcb0b61
Zach Davis <zach@castironcoding.com>
2014-03-24 20:21:20 -0700

    [CHORE] Adjusts production mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,9 +10,8 @@ Vocat::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.default_url_options = {:host => 'vocat.dev'}
   #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'vocat.dev'}
   config.action_mailer.delivery_method = :sendmail
 
   # Print deprecation notices to the Rails logger
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,9 @@ Vocat::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'vocat.io'}
+  config.action_mailer.delivery_method = :sendmail
 
   # Enable threaded mode
   # config.threadsafe!
