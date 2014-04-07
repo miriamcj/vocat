652f8192514ad1d70e2a23b61ef88c86231c8d7d
Zach Davis <zach@castironcoding.com>
2014-04-07 16:10:04 -0700

    [BUGFIX] Fixes #86: Some flash messages no longer display correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,18 +2,6 @@ module ApplicationHelper
 
   require 'active_support'
 
-  def form_errors(resource)
-    return unless resource.errors.messages.length > 0
-    if resource.errors.messages.length == 1
-      error = resource.errors.full_messages.first
-      return content_tag :div, error, :class => "alert alert-error"
-    end
-    p = content_tag :p, "Please fix the following errors:"
-    errors = resource.errors.full_messages.map { |msg| content_tag(:li, msg, nil, false) }.join
-    list = content_tag :ul, errors, nil, false
-    content_tag :div, p+list, :class => "alert alert-error alert-list"
-  end
-
   def serialize_for_bootstrap(data, current_user)
     if data.is_a?(Array)
       # See https://github.com/evrone/active_model_serializers/commit/22b6a74131682f086bd8095aaaf22d0cd6e8616d
