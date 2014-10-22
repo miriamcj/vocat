d71a7e7b68f736aeb5c97c62b771aca25442b93f
Zach Davis <zach@castironcoding.com>
2014-10-22 15:52:33 -0700

    [FEATURE] Ensures course requests display correctly in admin view

--- app/controllers/course_requests_controller.rb
+++ app/controllers/course_requests_controller.rb
@@ -3,13 +3,24 @@ class CourseRequestsController < ApplicationController
   load_and_authorize_resource :course_request
   respond_to :html
 
+
   def new
+    assign_years
     respond_with @course_request
   end
 
   def create
+    assign_years
     @course_request.evaluator = current_user
     flash[:notice] = "Your course request has been submitted." if @course_request.save
-    respond_with(@course_request, :location => dashboard_path, :action => :new)
+    respond_with(@course_request, :location => root_path, :action => :new)
   end
+
+  protected
+
+  def assign_years
+    y = Time.now.year
+    @years = (y..y+5).to_a
+  end
+
 end
