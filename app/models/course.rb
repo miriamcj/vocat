bbde48b59a314b2832007cf529eab3a01ab02a99
Zach Davis <zach@castironcoding.com>
2014-11-03 19:01:17 -0800

    [BUGFIX FIXES #82006720] Section in course search should be case insensitive

--- app/models/course.rb
+++ app/models/course.rb
@@ -31,7 +31,7 @@ class Course < ActiveRecord::Base
     c = Course.all
     c = c.where({department: params[:department]}) unless params[:department].blank?
     c = c.where({year: params[:year]}) unless params[:year].blank?
-    c = c.where({section: params[:section]}) unless params[:section].blank?
+    c = c.where("lower(section) = ?", params[:section].downcase) unless params[:section].blank?
     c = c.joins(:semester).where(:semesters => {id: params[:semester]}) unless params[:semester].blank?
     c = c.joins(:evaluators).where(:users => {id: params[:evaluator]}) unless params[:evaluator].blank?
     c.sorted
