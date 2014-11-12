af76a8fcaecd90411f3d7f3b60f04e5b51a8f425
Zach Davis <zach@castironcoding.com>
2014-11-12 14:04:50 -0800

    [FEATURE] Adds flash message to dashboards notifying users of upcoming downtime

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,9 @@ class DashboardController < ApplicationController
   skip_authorization_check
 
   def index
+    flash[:notice] = 'On Friday November 14th Vocat will be down for a scheduled upgrade from 11am
+    until approximately 1pm. When it\'s back online, it will have an entirely new design.
+    It will be awesome."'
     current_user.creator_courses.each do |course|
       factory = SubmissionFactory.new
       @submissions = factory.course_and_creator(course, current_user)
