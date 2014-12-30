9006c5b054aeae9a987002583d6ba62bda3ac281
Zach Davis <zach@castironcoding.com>
2014-12-29 16:47:07 -0800

    [CHORE] Adjusts ruby version for 3.1.2 release and adjusts staging deploy target

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,13 +1,13 @@
-set :branch, "zd/redesign"
-set :server_name, "vocat.dev.cichq.com"
+set :branch, "v3.1"
+set :server_name, "vocat.cic-stg.com"
 set :application, "vocat"
 
 set :stage, :production
 set :rails_env, :production
 
-role :app, %w{vocat@vocat.dev.cichq.com}
-role :web, %w{vocat@vocat.dev.cichq.com}
-role :db,  %w{vocat@vocat.dev.cichq.com}
+role :app, %w{vocat@vocat.cic-stg.com}
+role :web, %w{vocat@vocat.cic-stg.com}
+role :db,  %w{vocat@vocat.cic-stg.com}
 
 set :deploy_to, "~/#{fetch(:application)}"
 
