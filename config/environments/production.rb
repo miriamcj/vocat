e46a151984557ebf6aede446740533dc3302401c
Zach Davis <zach@castironcoding.com>
2015-02-04 16:50:22 -0800

    [BUGFIX] Fixes domain in default_url_options in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Vocat::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => 'app.vocat.io'}
+  config.action_mailer.default_url_options = {:host => 'baruch.vocat.io'}
   config.action_mailer.delivery_method = :sendmail
 
   # Enable threaded mode
