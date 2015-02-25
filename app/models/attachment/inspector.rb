023a6f7b9f07f7979a0f0c95cd46a4519ca19b51
Zach Davis <zach@castironcoding.com>
2015-02-25 09:38:29 -0800

    [FEATURE DELIVERS #88359704] Remove PDFs from allowed image types

--- app/models/attachment/inspector.rb
+++ app/models/attachment/inspector.rb
@@ -2,13 +2,13 @@ class Attachment::Inspector
 
   TYPES = {
     :audio => %w[mp3],
-    :image => %w[gif jpg jpeg png tif tiff pdf],
+    :image => %w[gif jpg jpeg png tif tiff],
     :video => %w[avi mp4 m4v mov flv wmv webm ogv]
   }
 
   MIME_TYPES = {
     :audio => %w[audio/mp3],
-    :image => %w[image/gif image/jpg image/jpeg image/png image/tiff application/pdf],
+    :image => %w[image/gif image/jpg image/jpeg image/png image/tiff],
     :video => %w[video/x-msvideo video/mp4 video/x-m4v quicktime/mov video/x-flv video/x-ms-wmv video/webm, video/ogg]
   }
 
