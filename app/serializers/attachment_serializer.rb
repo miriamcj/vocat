20dfd1667b0c93d4ab1e11d08e9aeac4d811a426
Zach Davis <zach@castironcoding.com>
2015-04-13 08:20:23 -0700

    [FEATURE] Attachment serializer now provides information about object storage

--- app/serializers/attachment_serializer.rb
+++ app/serializers/attachment_serializer.rb
@@ -2,7 +2,15 @@ require 'action_view'
 include ActionView::Helpers::NumberHelper
 
 class AttachmentSerializer < ActiveModel::Serializer
-  attributes :id, :current_user_can_destroy, :state, :s3_upload_document, :size, :extension
+  attributes :id, :current_user_can_destroy, :state, :s3_upload_document, :size, :extension, :bucket, :bucket_public_key
+
+  def bucket
+    Rails.application.config.vocat.aws[:s3_bucket]
+  end
+
+  def bucket_public_key
+    Rails.application.config.vocat.aws[:key]
+  end
 
   def s3_upload_document
     if object.committed?
