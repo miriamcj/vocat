d0bca1e435c257faa02d0af85fda9698b03f5140
Zach Davis <zach@castironcoding.com>
2015-04-23 12:16:55 -0700

    [FEATURE] Updates DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,12 +19,12 @@ ActiveRecord::Schema.define(version: 20150327200440) do
 
   create_table "annotations", force: :cascade do |t|
     t.text     "body"
-    t.string   "smpte_timecode",     limit: 255
+    t.string   "smpte_timecode"
     t.boolean  "published"
     t.float    "seconds_timecode"
     t.integer  "author_id"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "video_id_to_delete"
     t.integer  "asset_id"
     t.text     "canvas"
@@ -44,44 +44,44 @@ ActiveRecord::Schema.define(version: 20150327200440) do
 
   create_table "attachment_variants", force: :cascade do |t|
     t.integer  "attachment_id"
-    t.string   "location",        limit: 255
-    t.string   "format",          limit: 255
-    t.string   "state",           limit: 255
-    t.string   "processor_name",  limit: 255
+    t.string   "location"
+    t.string   "format"
+    t.string   "state"
+    t.string   "processor_name"
     t.text     "processor_data"
-    t.string   "processor_error", limit: 255
+    t.string   "processor_error"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "attachments", force: :cascade do |t|
-    t.string   "media_file_name",     limit: 255
-    t.string   "media_content_type",  limit: 255
+    t.string   "media_file_name"
+    t.string   "media_content_type"
     t.integer  "media_file_size"
     t.datetime "media_updated_at"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "state",               limit: 255
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "state"
     t.integer  "video_id_to_delete"
-    t.string   "processor_error",     limit: 255
+    t.string   "processor_error"
     t.integer  "user_id"
-    t.string   "processed_key",       limit: 255
-    t.string   "processor_job_id",    limit: 255
-    t.string   "processor_class",     limit: 255
-    t.string   "processed_thumb_key", limit: 255
+    t.string   "processed_key"
+    t.string   "processor_job_id"
+    t.string   "processor_class"
+    t.string   "processed_thumb_key"
     t.hstore   "processing_data"
     t.integer  "asset_id"
   end
 
   create_table "course_requests", force: :cascade do |t|
-    t.string   "name",         limit: 255
-    t.string   "department",   limit: 255
-    t.string   "section",      limit: 255
-    t.string   "number",       limit: 255
+    t.string   "name"
+    t.string   "department"
+    t.string   "section"
+    t.string   "number"
     t.integer  "year"
     t.integer  "semester_id"
     t.integer  "evaluator_id"
-    t.string   "state",        limit: 255
+    t.string   "state"
     t.integer  "admin_id"
     t.integer  "course_id"
     t.datetime "created_at"
@@ -89,15 +89,15 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   end
 
   create_table "courses", force: :cascade do |t|
-    t.string   "name",               limit: 255
-    t.string   "department",         limit: 255
-    t.string   "number",             limit: 255
-    t.string   "section",            limit: 255
+    t.string   "name"
+    t.string   "department"
+    t.string   "number"
+    t.string   "section"
     t.text     "description"
     t.integer  "organization_id"
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.hstore   "settings_to_delete",             default: {}, null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.hstore   "settings_to_delete", default: {}, null: false
     t.text     "message"
     t.integer  "semester_id"
     t.integer  "year"
@@ -130,17 +130,17 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   add_index "courses_evaluators_to_be_deleted", ["user_id"], name: "index_courses_evaluators_to_be_deleted_on_user_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",               default: 0
-    t.integer  "attempts",               default: 0
+    t.integer  "priority",   default: 0
+    t.integer  "attempts",   default: 0
     t.text     "handler"
     t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by",  limit: 255
-    t.string   "queue",      limit: 255
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
@@ -171,10 +171,10 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   add_index "evaluations", ["scores"], name: "index_evaluations_on_scores", using: :btree
 
   create_table "groups", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.integer  "course_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "groups_creators", id: false, force: :cascade do |t|
@@ -185,7 +185,7 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   create_table "memberships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "course_id"
-    t.string   "role",       limit: 255
+    t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -230,40 +230,40 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
 
   create_table "organizations", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "project_types", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string   "name",                        limit: 255
+    t.string   "name"
     t.text     "description"
     t.integer  "course_id"
     t.integer  "project_type_id"
-    t.datetime "created_at",                                               null: false
-    t.datetime "updated_at",                                               null: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
     t.integer  "rubric_id"
     t.integer  "listing_order"
-    t.string   "type",                        limit: 255, default: "user"
+    t.string   "type",                        default: "user"
     t.date     "due_date"
-    t.text     "allowed_attachment_families",             default: [],                  array: true
-    t.hstore   "settings",                                default: {},     null: false
+    t.text     "allowed_attachment_families", default: [],                  array: true
+    t.hstore   "settings",                    default: {},     null: false
   end
 
   add_index "projects", ["course_id"], name: "index_projects_on_course_id", using: :btree
   add_index "projects", ["project_type_id"], name: "index_projects_on_project_type_id", using: :btree
 
   create_table "rubrics", force: :cascade do |t|
-    t.string   "name",            limit: 255
+    t.string   "name"
     t.boolean  "public"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "owner_id"
     t.text     "description"
     t.integer  "organization_id"
@@ -276,23 +276,23 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   end
 
   create_table "semesters", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "submissions", force: :cascade do |t|
-    t.string   "name",                   limit: 255
+    t.string   "name"
     t.text     "summary"
     t.integer  "project_id"
     t.integer  "creator_id"
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.boolean  "published"
-    t.integer  "discussion_posts_count",             default: 0
-    t.string   "creator_type",           limit: 255, default: "User"
-    t.integer  "assets_count",                       default: 0
+    t.integer  "discussion_posts_count", default: 0
+    t.string   "creator_type",           default: "User"
+    t.integer  "assets_count",           default: 0
   end
 
   add_index "submissions", ["creator_id"], name: "index_submissions_on_creator_id", using: :btree
@@ -309,31 +309,31 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "role",                   limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "role"
     t.integer  "organization_id"
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "middle_name",            limit: 255
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "middle_name"
     t.text     "settings"
-    t.string   "org_identity",           limit: 255
-    t.string   "gender",                 limit: 255
-    t.string   "city",                   limit: 255
-    t.string   "state",                  limit: 255
-    t.string   "country",                limit: 255
+    t.string   "org_identity"
+    t.string   "gender"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
     t.boolean  "is_ldap_user"
-    t.hstore   "preferences",                        default: {}, null: false
+    t.hstore   "preferences",            default: {}, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
@@ -341,10 +341,10 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
-    t.string   "item_type",  limit: 255, null: false
-    t.integer  "item_id",                null: false
-    t.string   "event",      limit: 255, null: false
-    t.string   "whodunnit",  limit: 255
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
   end
@@ -352,15 +352,15 @@ ActiveRecord::Schema.define(version: 20150327200440) do
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
   create_table "videos_to_delete", force: :cascade do |t|
-    t.string   "name",          limit: 255
-    t.string   "url",           limit: 255
-    t.string   "source_id",     limit: 255
-    t.string   "source_url",    limit: 255
-    t.string   "source",        limit: 255
+    t.string   "name"
+    t.string   "url"
+    t.string   "source_id"
+    t.string   "source_url"
+    t.string   "source"
     t.integer  "submission_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "thumbnail_url", limit: 255
+    t.string   "thumbnail_url"
   end
 
 end
