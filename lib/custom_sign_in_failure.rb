9c39ec514ee9033cb06c583e6f640f292d8a456a
Zach Davis <zach@castironcoding.com>
2015-04-25 11:01:51 -0700

    [BUGFIX] Finishes custom failure app so that oauth token requests return a 401 header if login fails

--- lib/custom_sign_in_failure.rb
+++ lib/custom_sign_in_failure.rb
@@ -9,6 +9,10 @@ class CustomSignInFailure < Devise::FailureApp
     Rails.logger.info "########## CUSTOM FAILURE APP CALLED"
     if !attempted_path.start_with?('/oauth')
       redirect
+    else
+      self.status = 401
+      self.content_type = request.format.to_s
+      self.response_body = '{"errors": ["Vocat wasn\'t able to find an account with this username and password. Please re-enter and try again."]}'
     end
   end
 end
