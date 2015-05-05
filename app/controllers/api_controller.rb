05ffe6f4ec1638d8aeacac01ecb32a0096db05a9
Zach Davis <zach@castironcoding.com>
2015-05-05 14:39:26 -0700

    [BUGFIX] Catch common exceptions and return correct response in api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,4 +4,12 @@ class ApiController < ApplicationController
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, :if => lambda { request.headers[:Authorization] }
 
+  rescue_from CanCan::AccessDenied do |exception|
+    render nothing: true, status: :forbidden
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render nothing: true, status: :not_found
+  end
+
 end
