9be3bff8bf00a8c5bbf6036d1dc00752f8aafba7
Zach Davis <zach@castironcoding.com>
2015-05-06 17:02:14 -0700

    [FEATURE] Finishes submissions and user API docs

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -4,7 +4,52 @@ class Api::V1::SubmissionsController < ApiController
   skip_authorization_check # Authorization is more complex on this controller, so we do it in each individual action
   load_resource :submission
 
-  # GET /api/v1/submissions/for_course.json?course=:course
+  resource_description do
+    description <<-EOS
+      Submissions tie together the media, discussion posts, and evaluations that are created in response to a student's work
+      on a project. Submissions are created automatically, on the fly by Vocat. Vocat will always return a one submission
+      for every group and student that can submit for a given project. The existence of a submission, then, should not
+      be taken to mean that a student has taken action on a project. For performance reasons, when the API renders
+      collections of submissions, it uses a brief serializer that does not include all submission details. To access the
+      full details of a submission, API clients should request the single submission.
+    EOS
+  end
+
+  api :GET, '/submission/for_course?course=:course', "returns summaries of all submissions for the given course"
+  param :course, Fixnum, :desc => "The course ID", :required => true
+  example <<-EOF
+    Sample Response:
+
+    [
+      {
+        "id": 7395,
+        "creator_id": 4483,
+        "project_id": 466,
+        "creator_type": "User",
+        "serialized_state": "partial",
+        "current_user_published": null,
+        "current_user_percentage": null,
+        "instructor_score_percentage": "68.8888888888889",
+        "role": "administrator",
+        "has_asset": true,
+        "list_name": "Blick, Betty"
+      },
+      {
+        "id": 7398,
+        "creator_id": 4519,
+        "project_id": 466,
+        "creator_type": "User",
+        "serialized_state": "partial",
+        "current_user_published": null,
+        "current_user_percentage": null,
+        "instructor_score_percentage": null,
+        "role": "administrator",
+        "has_asset": false,
+        "list_name": "Reinger, Ebony"
+      },
+      [...]
+    ]
+  EOF
   def for_course
     factory = SubmissionFactory.new
     @course = Course.find(params.require(:course))
@@ -13,7 +58,63 @@ class Api::V1::SubmissionsController < ApiController
     respond_with @submissions, :each_serializer => BriefSubmissionSerializer
   end
 
-  # GET /api/v1/submissions/for_creator_and_project.json?creator=:user_or_group&project=:project&creator_type=user_or_group
+
+
+  api :GET, '/submission/for_creator_and_project?creator=:creator&project=:project&creator_type=:creator_type', "returns detailed submissions for the given creator and project"
+  param :creator, Fixnum, :desc => "The group or course ID", :required => true
+  param :project, Fixnum, :desc => "The project ID", :required => true
+  param :creator_type, ['User','Group'], :desc => "A string telling Vocat whether to return a user or group creator", :required => true
+  description "The following example is truncated, as the serialized submission also includes the project, the rubric, all evaluations, and all assets associated with the submission. To see what fields are exposed on these resources, please consult the related API doc pages."
+  example <<-EOF
+    Sample Response:
+
+    [
+      {
+        "id":7395,
+        "name":null,
+        "path":"/courses/234/users/evaluations/creator/4483/project/466",
+        "serialized_state":"full",
+        "role":"administrator",
+        "discussion_posts_count":1,
+        "new_posts_for_current_user":false,
+        "project":{[SEE PROJECT RESOURCE]},
+        "creator":{
+          "id":4483,
+          "email":"creator10@test.com",
+          "name":"Betty Blick",
+          "gravatar":"http://gravatar.com/avatar/8a4e59d9970b880e602cd29d77fc2fd7.png?d=mm\u0026s=",
+          "first_name":"Betty",
+          "last_sign_in_at":null,
+          "org_identity":"95661757",
+          "list_name":"Blick, Betty"
+        },
+        "creator_id":4483,
+        "creator_type":"User",
+        "project_id":466,
+        "evaluations":[
+          {[SEE EVALUATION RESOURCE]}
+        ],
+        "abilities":{
+          "can_own":true,
+          "can_evaluate":false,
+          "can_attach":true,
+          "can_discuss":true,
+          "can_annotate":true,
+          "can_administer":true
+        },
+        "current_user_published":null,
+        "current_user_percentage":null,
+        "evaluated_by_peers":false,
+        "peer_score_percentage":0,
+        "evaluated_by_instructor":true,
+        "instructor_score_percentage":"68.8888888888889",
+        "has_asset":true,
+        "assets":[
+          {[SEE ASSET RESOURCE]}
+        ]
+      }
+    ]
+  EOF
   def for_creator_and_project
     factory = SubmissionFactory.new
     creator_type = params.require(:creator_type)
@@ -34,8 +135,13 @@ class Api::V1::SubmissionsController < ApiController
     respond_with @submissions
   end
 
-  # GET /api/v1/submissions/for_course_and_user.json?course=:course&user=:user&project=:project
-  # Note that project is optional.
+
+
+  api :GET, '/submission/for_course_and_user?course=:course&project=:project&user=:user', "returns detailed submissions for the given user, course, and project (optional)"
+  param :user, Fixnum, :desc => "The user ID", :required => true
+  param :project, Fixnum, :desc => "The project ID"
+  param :course, Fixnum, :desc => "The course ID", :required => true
+  description "For a sample of the response, see the for_creator_and_project endpoint, above."
   def for_course_and_user
     factory = SubmissionFactory.new
     @course = Course.find(params.require(:course))
@@ -52,7 +158,11 @@ class Api::V1::SubmissionsController < ApiController
     respond_with @submissions
   end
 
-  # GET /api/v1/submissions/for_group.json?group=:group
+
+
+  api :GET, '/submission/for_group?group=:group', "returns detailed submissions for the given group"
+  param :group, Fixnum, :desc => "The group ID", :required => true
+  description "For a sample of the response, see the for_creator_and_project endpoint, above."
   def for_group
     factory = SubmissionFactory.new
     @group = Group.find(params.require(:group))
@@ -61,7 +171,11 @@ class Api::V1::SubmissionsController < ApiController
     respond_with @submissions
   end
 
-  # GET /api/v1/submissions/for_project.json?project=:project&brief=false
+
+
+  api :GET, '/submission/for_project?project=:project', "returns brief submissions the given project"
+  param :project, Fixnum, :desc => "The project ID"
+  description "For a sample of the response, see the for_creator_and_project endpoint, above."
   def for_project
     params[:brief] ? brief = true : brief = false
     factory = SubmissionFactory.new
@@ -75,13 +189,16 @@ class Api::V1::SubmissionsController < ApiController
     end
   end
 
-  # GET /api/v1/submissions/:id.json
+  api :GET, '/submission/:id', "shows a single submission"
+  param :id, Fixnum, :desc => "The submission ID", :required => true
+  description "For a sample of the response, see the for_creator_and_project endpoint, above."
   def show
     authorize! :show, @submission
     respond_with @submission, :root => false
   end
 
-  # POST /api/v1/courses/:course_id/submissions.json
+
+  api :POST, "/course/:course_id/submissions", "creates a submission", :deprecated => true
   def create
     authorize! :create, @submission
     if @submission.save
@@ -91,14 +208,17 @@ class Api::V1::SubmissionsController < ApiController
     end
   end
 
-  # PUT /api/v1/submissions/:id.json
+  api :PATCH, "/submissions/:id", "updates a submission", :deprecated => true
   def update
     authorize! :update, @submission
     @submission.update_attributes!(submission_params)
     respond_with(@submission)
   end
 
-  # DELETE /api/v1/submissions/:id
+
+  api :DELETE, '/submissions/:id', "deletes a submission and all attached media, annotations, discussion posts, and evaluations."
+  param :id, Fixnum, :desc => "The submission ID"
+  error :code => 404, :desc => "The submission could not be found."
   def destroy
     authorize! :destroy, @submission
     @submission.destroy
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,6 +11,22 @@ class Api::V1::UsersController < ApiController
   end
 
   # /api/v1/users/1
+  api :GET, '/users/:id', "shows a user"
+  param :id, Fixnum, :desc => "The ID of the user to be shown"
+  example <<-EOF
+    Sample Response:
+
+    {
+      "id": 4470,
+      "email": "evaluator1@test.com",
+      "name": "Ressie Crona",
+      "gravatar": "http://gravatar.com/avatar/4c3fe1bdae8ec9e5db97d02aa4cf1cda.png?d=mm&s=",
+      "first_name": "Ressie",
+      "last_sign_in_at": "2015-05-06T17:51:54.327-04:00",
+      "org_identity": "30683835",
+      "list_name": "Crona, Ressie"
+    }
+  EOF
   def show
     respond_with @user
   end
@@ -27,6 +43,22 @@ class Api::V1::UsersController < ApiController
   end
 
   # /api/v1/users/me
+  api :GET, '/users/me', "shows the current authenticated user"
+  error :code => 401, :desc => "Your API token has likely expired."
+  example <<-EOF
+    Sample Response:
+
+    {
+      "id": 4470,
+      "email": "evaluator1@test.com",
+      "name": "Ressie Crona",
+      "gravatar": "http://gravatar.com/avatar/4c3fe1bdae8ec9e5db97d02aa4cf1cda.png?d=mm&s=",
+      "first_name": "Ressie",
+      "last_sign_in_at": "2015-05-06T17:51:54.327-04:00",
+      "org_identity": "30683835",
+      "list_name": "Crona, Ressie"
+    }
+  EOF
   def me
     respond_with current_user
   end
