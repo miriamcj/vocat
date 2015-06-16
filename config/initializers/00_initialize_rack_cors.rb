64e169be3edf8b76f198f674fd223055d850cfcc
Zach Davis <zach@castironcoding.com>
2015-06-16 13:50:12 -0700

    [CHORE] Disables Rack Cors, which is not needed for now

--- config/initializers/00_initialize_rack_cors.rb
+++ config/initializers/00_initialize_rack_cors.rb
@@ -1,13 +1,13 @@
-module Vocat
-  class Application < Rails::Application
-    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => Rails.logger do
-      allow do
-        origins 'localhost:8100'
-        resource '*',
-                 :headers => :any,
-                 :methods => [:get, :post, :delete, :put, :options, :head],
-                 :max_age => 0
-      end
-    end
-  end
-end
+# module Vocat
+#   class Application < Rails::Application
+#     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => Rails.logger do
+#       allow do
+#         origins 'localhost:8100'
+#         resource '*',
+#                  :headers => :any,
+#                  :methods => [:get, :post, :delete, :put, :options, :head],
+#                  :max_age => 0
+#       end
+#     end
+#   end
+# end
