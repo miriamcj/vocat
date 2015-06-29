7e756c17f2256ad3ab67c438ad0d2fd4af6ee2fc
Zach Davis <zach@castironcoding.com>
2015-06-29 10:40:18 -0700

    [BUGFIX FIXES #154] Resolves exception in creator dashboard by requiring authenticated user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,9 @@
 class DashboardController < ApplicationController
 
   skip_authorization_check
+  before_action :authenticate_user!
 
   def evaluator
-    # Not sure why this is necessary... -ZD
-    # current_user.creator_courses.each do |course|
-    #   factory = SubmissionFactory.new
-    #   @submissions = factory.course_and_creator(course, current_user)
-    # end
   end
 
   def creator
