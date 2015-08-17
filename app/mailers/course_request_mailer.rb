e490c4c7cba853c64d29557239f00ff1eb8c94fe
Zach Davis <zach@castironcoding.com>
2015-08-17 13:47:45 -0700

    [BUGFIX] Makes course request mailer more tolerante of missing from and to email addresses

--- app/mailers/course_request_mailer.rb
+++ app/mailers/course_request_mailer.rb
@@ -4,19 +4,35 @@ class CourseRequestMailer < ActionMailer::Base
   def create_notify_email(course_request)
     @course_request = course_request
     @organization = @course_request.organization
-    mail(to: @organization.email_notification_course_request, from: from(@organization), subject: 'A course request has been submitted')
+    from = from(@organization)
+    to = @organization.email_notification_course_request
+    if to.blank? || from.blank?
+      return false
+    end
+    mail(to: to, from: from, subject: 'A course request has been submitted')
   end
 
   def approve_notify_email(course_request)
     @course_request = course_request
     @organization = @course_request.organization
-    mail(to: course_request.evaluator_email, from: from(@organization), subject: 'Your course request has been approved')
+    from = from(@organization)
+    to = course_request.evaluator_email
+    if to.blank? || from.blank?
+      return false
+    end
+    mail(to: to, from: from, subject: 'Your course request has been approved')
   end
 
   def deny_notify_email(course_request)
     @course_request = course_request
     @organization = @course_request.organization
-    mail(to: course_request.evaluator_email, from: from(@organization), subject: 'Your course request has been denied')
+    from = from(@organization)
+    to = course_request.evaluator_email
+    if to.blank? || from.blank?
+      return false
+    end
+
+    mail(to: to, from: from, subject: 'Your course request has been denied')
   end
 
   protected
