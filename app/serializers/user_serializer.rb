d092a860b42512edb46d234a2bdd02740a3929f1
Zach Davis <zach@castironcoding.com>
2018-02-02 11:13:46 -0800

    [B] Request secure gravatar

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -41,7 +41,7 @@ class UserSerializer < ActiveModel::Serializer
 
   def gravatar
     gravatar_id = Digest::MD5.hexdigest(object.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?d=mm&s="
+    "https://gravatar.com/avatar/#{gravatar_id}.png?d=mm&s="
   end
 
 end
