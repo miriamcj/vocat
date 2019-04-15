dfbd3791c00099189a2a59f947001f14a571e806
Dana Johnson <dana@castironcoding.com>
2019-04-15 13:41:02 -0700

    [C] Add bulk-decaffeinate config

---
+++ app/assets/bulk-decaffeinate.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  searchDirectory: "./javascripts/app",
+  outputFileExtension: "js"
+  // useJSModules: true,
+  // jscodeshiftScripts: [
+  //   // './node_modules/5to6-codemod/transforms/cjs.js',
+  // ],
+  // fixImportsConfig: {
+  //   searchPath: './javascripts/app',
+  //   absoluteImportPaths: ['./javascripts/app'],
+  // },
+};
